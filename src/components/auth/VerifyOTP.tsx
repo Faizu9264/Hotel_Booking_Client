5c0fe204fee96c22fa954965dc7609374562b22b
FaizuRahman <www.faizu9264@gmail.com>
2023-11-21 13:09:39 +0530

    verify otp

--- src/components/auth/VerifyOTP.tsx
+++ src/components/auth/VerifyOTP.tsx
@@ -1,12 +1,12 @@
-// // VerifyOTP.tsx
+
+
 // import React, { useState } from 'react';
 // import { useLocation, useNavigate } from 'react-router-dom';
 // import api from '../../services/api';
 // import OTPModal from '../common/OTPModal';
 // import { useDispatch, useSelector } from 'react-redux';
-// import  {RootState}  from '../../redux/store';
+// import { RootState } from '../../redux/store';
 // import { toast } from 'react-toastify';
-// // import LoginModal from '../common/LoginModal';
 
 // interface VerifyOTPProps {
 //   onRequestClose: (modalIdentifier: string) => void;
@@ -18,29 +18,40 @@
 //   const user = authState.user;
 //   const [enteredOtp, setEnteredOtp] = useState('');
 //   const [error, setError] = useState<string | null>(null);
+//   const [isResending, setResending] = useState(false); 
 //   const navigate = useNavigate();
 //   const location = useLocation();
-
+  
+  
 //   const handleVerify = async () => {
 //     try {
 //       console.log(user);
-//       // const { email } = location.state as { email: string };
-
 //       console.log(user.email, enteredOtp);
 //       const response = await api.verifyOTP(enteredOtp);
-//       // console.log('OTP verification successful', response);
 
-//     const completed =  await api.completeSignup();
-//       // dispatch(clearUserData());
+//       const completed = await api.completeSignup();
 
-//       if (completed && response) {   
-//        toast.success('OTP verification successful')
-//         navigate('/login',user.email);
+//       if (completed && response) {
+//         toast.success('OTP verification successful');
+//         navigate('/login', user.email);
 //       } else {
 //         setError('Invalid OTP. Please try again.');
 //       }
 //     } catch (error: any) {
 //       setError(error.message || 'Error verifying OTP');
+//     }
+//   };
+
+//   const handleResendOTP = async () => {
+//     try {
+//       setResending(true);
+//       await api.resendOTP(user.email);
+//       toast.success('OTP resent successfully');
+//       setResending(false);
+//     } catch (error) {
+//       console.error('Error resending OTP:', error);
+//       toast.error('Error resending OTP');
+//       setResending(false);
 //     }
 //   };
 
@@ -74,9 +85,18 @@
 //           >
 //             Verify OTP
 //           </button>
+//           <button
+//             type="button"
+//             onClick={handleResendOTP}
+//             disabled={isResending}
+//             className={`w-full px-4 py-2 text-white font-medium bg-gray-500 hover:bg-gray-600 ${
+//               isResending ? 'cursor-not-allowed' : 'hover:scale-105 active:bg-gray-600'
+//             } rounded-lg duration-150`}
+//           >
+//             {isResending ? 'Resending OTP...' : 'Resend OTP'}
+//           </button>
 //         </form>
 //       </div>
-//       {/* <LoginModal isOpen={loginModalOpen} onRequestClose={closeLoginModal}></LoginModal> */}
 //     </main>
 //   );
 // };
@@ -84,7 +104,8 @@
 // export default VerifyOTP;
 
 
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 import OTPModal from '../common/OTPModal';
@@ -102,10 +123,28 @@ const VerifyOTP: React.FC<VerifyOTPProps> = ({ onRequestClose }) => {
   const user = authState.user;
   const [enteredOtp, setEnteredOtp] = useState('');
   const [error, setError] = useState<string | null>(null);
-  const [isResending, setResending] = useState(false); // Add state to track resending status
+  const [isResending, setResending] = useState(false);
+  const [countdown, setCountdown] = useState(120); 
+  const [countdownTimer, setCountdownTimer] = useState<NodeJS.Timeout | null>(null);
   const navigate = useNavigate();
   const location = useLocation();
 
+  useEffect(() => {
+    let countdownTimer: NodeJS.Timeout;
+  
+    if (countdown > 0) {
+      // Start the countdown timer when the component mounts
+      countdownTimer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    }
+  
+    // Clear the countdown timer when the component unmounts or countdown reaches 0
+    return () => {
+      clearInterval(countdownTimer);
+    };
+  }, [countdown]);
+  
   const handleVerify = async () => {
     try {
       console.log(user);
@@ -127,10 +166,13 @@ const VerifyOTP: React.FC<VerifyOTPProps> = ({ onRequestClose }) => {
 
   const handleResendOTP = async () => {
     try {
-      setResending(true);
-      await api.resendOTP(user.email);
-      toast.success('OTP resent successfully');
-      setResending(false);
+      if (!isResending && countdown === 0) {
+        setResending(true);
+        await api.resendOTP(user.email);
+        toast.success('OTP resent successfully');
+        setResending(false);
+        setCountdown(120); 
+      }
     } catch (error) {
       console.error('Error resending OTP:', error);
       toast.error('Error resending OTP');
@@ -171,12 +213,14 @@ const VerifyOTP: React.FC<VerifyOTPProps> = ({ onRequestClose }) => {
           <button
             type="button"
             onClick={handleResendOTP}
-            disabled={isResending}
+            disabled={isResending || countdown > 0}
             className={`w-full px-4 py-2 text-white font-medium bg-gray-500 hover:bg-gray-600 ${
-              isResending ? 'cursor-not-allowed' : 'hover:scale-105 active:bg-gray-600'
+              isResending || countdown > 0
+                ? 'cursor-not-allowed'
+                : 'hover:scale-105 active:bg-gray-600'
             } rounded-lg duration-150`}
           >
-            {isResending ? 'Resending OTP...' : 'Resend OTP'}
+            {isResending ? 'Resending OTP...' : countdown > 0 ? `Resend OTP (${countdown}s)` : 'Resend OTP'}
           </button>
         </form>
       </div>
@@ -185,9 +229,6 @@ const VerifyOTP: React.FC<VerifyOTPProps> = ({ onRequestClose }) => {
 };
 
 export default VerifyOTP;
-
-
-
 
 
 
